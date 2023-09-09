cdfafef364e64fb2bc2e2b1a63fb1cbd2f66f0a6
alanscheibler <amscheibler@gmail.com>
2023-09-09 11:41:56 -0300

    Primeiro Commit

---
+++ main.py
@@ -0,0 +1,30 @@
+import os
+from functions import cld,
+
+cls()      
+
+input("-*-*-       Bem-vindo ao Space Transmiter!       -*-*-\n\n       Pressione qualquer tecla para continuar.\n")
+cls()
+
+options = print ("Menu:",
+       "\n    1 - Cadastrar Sonda e Gerar Par de Chaves.",
+       "\n    2 - Enviar Chave da Sonda.",
+       "\n    3 - Coletar dados da sonda.",
+       "\n    4 - Gerar Assinatura dos dados Coletados."
+       "\n    5 - Enviar para a Terra os dados.")
+def menu():
+       try:
+              if options == "1":
+                     print()
+              elif options == "2":
+                     print()
+              elif options == "3":
+                     print()
+              elif options == "4":
+                     print()
+              elif options == "5":
+                     print()
+       except:
+              print("Insira uma opção valida!")
+
+
