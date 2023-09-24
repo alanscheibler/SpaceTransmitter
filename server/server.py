d83dece788aeea85f347f09d497f4033024828d8
alanscheibler <amscheibler@gmail.com>
2023-09-24 16:03:19 -0300

    adição da verificação dos arquivos de registro  com a assinatura e a chave pública

--- server/server.py
+++ server/server.py
@@ -1,6 +1,7 @@
 import socket
 import threading
 import os
+import rsa
 
 HOST = '127.0.0.1'
 PORT = 443
@@ -12,11 +13,35 @@ serverSocket.listen()
 
 print('Aguardando conexões...')
 
+def verifySignature(sondaName, registerName):
+    signaturePath = os.path.join(sondaName, f'{registerName}.signature.txt')
+    registerPath = os.path.join(sondaName, f'{registerName}.txt')
+    publicKeyPath = os.path.join(sondaName, f'{sondaName}.public.pem')
+
+    with open(publicKeyPath, 'rb') as publicKeyFile:
+        publicKeyData = publicKeyFile.read()
+        publicKey = rsa.PublicKey.load_pkcs1(publicKeyData)
+    with open(registerPath, 'rb') as registerFile:
+        registerData = registerFile.read()
+    with open(signaturePath, 'rb') as signatureFile:
+        signatureData = signatureFile.read()
+
+    try:
+        rsa.verify(registerData, signatureData, publicKey)
+        print('Assinatura verificada com sucesso!')
+        return True
+    except Exception as e:
+        print('Falha na verificação da assinatura: ', str(e))
+        return False
+    
+verify = 0
 def handleClient(clientSocket):
+    global verify
     while True:
         try:       
             clientSocket.settimeout(60)
             message = clientSocket.recv(1024).decode()
+            print(verify)
 
             data = message.split('#')
             sondaName = data[0]
@@ -28,7 +53,7 @@ def handleClient(clientSocket):
                 print('Tipo do redistro: ', fileType)
             else:
                 print('Nome do registro: ', registerName,'\n'
-                    'Tipo do redistro: ', fileType) 
+                    'Tipo do registro: ', fileType) 
             
             sondaFolder = os.path.join(sondaName)
             if not os.path.exists(sondaFolder):
@@ -47,19 +72,25 @@ def handleClient(clientSocket):
                 print(f'Chave pública da sonda {sondaName} recebida e salva com sucesso')
             
             elif fileType == 'REGISTER':
+                verify = verify + 1
+                print(verify)
                 registerData = clientSocket.recv(4096)
                 if registerData:
                     with open(os.path.join(sondaFolder,f'{registerName}.txt'), 'wb') as regFile: 
                         regFile.write(registerData)
-                    print('Arquivo de registro recebido e salvo com sucesso!')
+                    
                 
             elif fileType == 'SIGNATURE':
+                verify = verify + 1
+                print(verify)
                 signatureData = clientSocket.recv(4096)
                 if signatureData:
                     with open(os.path.join(sondaFolder,f'{registerName}.signature.txt'), 'wb') as sigFile: 
                         sigFile.write(signatureData)
-                    print('Arquivo de assinatura recebido e salvo com sucesso!')
-                    
+
+            if verify == 2:
+                verify == 0       
+                verifySignature(sondaName, registerName)
         except socket.timeout:
             print('Tempo limite atingido. Fechando conexão')
             break
